d61c6792556a309ea6e864913d73fe6a71a8287e
Govinda945 <95563153+Govinda945@users.noreply.github.com>
2023-05-22 17:01:42 +0530

    commit temp77

---
+++ temp77/main.tf
@@ -0,0 +1,23 @@
+provider "aws" { 
+ 	 region = "ap-south-1" 
+}
+
+resource "aws_autoscalingplans_scaling_plan" "asp366" {
+  name = "ssap"
+  application_source = "dsf"
+  scaling_instruction = "sdfds"
+  key = "sdfds"
+  max_capacity = "sdf"
+  min_capacity = "sdfsd"
+  resource_id = "wrge3r"
+  scalable_dimension = "wqrf"
+  service_namespace = "sdf"
+  target_tracking_configuration = "gswr"
+  metric_name = "hrewr"
+  namespace = "h34wr"
+  statistic = "g4wr"
+  predefined_load_metric_type = "resfd"
+  target_value = "g4wrae"
+  predefined_scaling_metric_type = "gwesd"
+}
+ 
